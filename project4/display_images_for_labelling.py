20d1c8b64186cbb7262f8fdcf8316a14fba5e0c3
Ryan Dalby <dalbyryan3@gmail.com>
2021-12-01 13:20:54 -0700

    Added better correspondence labelling

--- project4/display_images_for_labelling.py
+++ project4/display_images_for_labelling.py
@@ -2,21 +2,31 @@ import os
 import matplotlib.pyplot as plt
 from skimage import io
 
-
-def display_all_png_in_dir(dir):
-    for filename in os.listdir(dir):
-        if not filename.endswith('.png'):
-            continue
-        img = io.imread('{0}'.format('{0}{1}'.format(dir,filename)))
-        plt.figure()
-        plt.imshow(img)
+def onclick(event, click_list, row_col_order=False):
+    if event.dblclick:
+        if row_col_order:
+            click_list.append([round(event.ydata), round(event.xdata)])
+        else:
+            click_list.append([round(event.xdata), round(event.ydata)])
+def display_img_and_connect_onclick(dir, filename, filename_click_dict):
+    img = io.imread('{0}'.format(os.path.join(dir,filename)))
+    filename_click_dict[filename] = []
+    fig = plt.figure()
+    fig.canvas.mpl_connect('button_press_event', lambda x: onclick(x, filename_click_dict[filename], row_col_order=False)) 
+    plt.imshow(img)
     plt.show(block=False)
 
-dirs = ['fruit_images/', 'president_images/']
-for dir in dirs:
-    display_all_png_in_dir(dir)
+dir = input('Enter path to directory to look at ') 
+print('Double click on an image to add a point to the final output associated with that image')
+filename_click_dict = {}
+for filename in os.listdir(dir):
+    if not (filename.endswith('.png') or filename.endswith('.tif') or filename.endswith('.tiff')):
+        continue
+    display_img_and_connect_onclick(dir, filename, filename_click_dict)
+
 print('Exit all by keyboard press on newest figure')
 while True:
     if plt.waitforbuttonpress(0) == True:
         plt.close('all')
         break
+print(filename_click_dict)
