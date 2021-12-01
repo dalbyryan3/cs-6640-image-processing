dfe9282be288e304b5a105bce3f8c8e0072286ec
Ryan Dalby <dalbyryan3@gmail.com>
2021-12-01 09:47:41 -0700

    Implemented vectorized rbf functions

--- project4/project4.py
+++ project4/project4.py
@@ -85,20 +85,70 @@ morph_fruits_input_filenames, morph_fruits_correspondences_dict, morph_fruits_ou
 morph_pres_input_filenames, morph_pres_correspondences_dict, morph_pres_output_filename = visualize_correspondences('{0}{1}'.format(president_img_dir, morph_params_filename), figsize=(15,5), color=['red','green','blue','black', 'gray', 'maroon', 'darkorange', 'cyan', 'magenta', 'lightgreen','wheat','olive','steelblue','lavender','lime','lightcoral','yellow','pink','deepskyblue','springgreen'], suptitle_text='Morphing Presidents Correspondences', suptitle_fontsize=20)
 
 # %%
-def rbf_thin_plate_splines(xy, xy_corr):
+def calc_rbf_pairwise_dist_sq(X, Y):
+    # Using inspiration from https://stackoverflow.com/questions/47271662/what-is-the-fastest-way-to-compute-an-rbf-kernel-in-python 
+    # data_point_coords is a mxd ndarray
+    # data_point_vals is a nxd ndarray
+    # Vectorized, specifically using einstein summing notation (indices are like "dummy" variables, shared index in multiply implies a sum, free subscript indicates and index)
+    # Also squared pairwise distance was formed as a vector operation
+
+    # Gives pairwise distance squared for all i,j in X against Y
+    X_norm = np.einsum('ij,ij->i', X, X)
+    Y_norm = np.einsum('ij,ij->i', Y, Y)
+
+    rbf_pairwise_dist_sq = X_norm.reshape((-1,1)) + Y_norm.reshape((1,-1)) - 2 * np.dot(X, Y.T) # size mxn, corresponding pairwise distance for any i,j in the matrix
+    return rbf_pairwise_dist_sq
+
+def rbf_thin_plate_splines(data_point_coords, data_point_vals):
+    # data_point_coords is ndarray [(x1,y1),...,(xm,ym)], # mx2
+    # data_point_vals is ndarray [(x1,y1),...,(xn,yn)] # nx2
+    # Gives phi as ndarray for all i,j in data_point_coords against data_point_vals
+    rbf_pairwise_dist_sq = calc_rbf_pairwise_dist_sq(data_point_coords, data_point_vals)
+    phi_mat = np.full(rbf_pairwise_dist_sq.shape, -1.0) # For all places where rbf_pairwise_dist_sq is 0 limit evaluates to -1
+    rbf_pairwise_dist_sq_nonzero_mask = rbf_pairwise_dist_sq != 0.0
+    phi_mat[rbf_pairwise_dist_sq_nonzero_mask] = rbf_pairwise_dist_sq[rbf_pairwise_dist_sq_nonzero_mask] * np.log(np.sqrt(rbf_pairwise_dist_sq[rbf_pairwise_dist_sq_nonzero_mask]))
+    return phi_mat
+
+def rbf_gaussian(data_point_coords, data_point_vals, sigma):
+    # data_point_coords is ndarray [(x1,y1),...,(xm,ym)], # mx2
+    # data_point_vals is ndarray [(x1,y1),...,(xn,yn)] # nx2
+    # Gives phi as ndarray for all i,j in data_point_coords against data_point_vals
+    rbf_pairwise_dist_sq = calc_rbf_pairwise_dist_sq(data_point_coords, data_point_vals)
+    return np.exp(-rbf_pairwise_dist_sq/(2*sigma**2))
+
+def rbf_thin_plate_splines_naive(xy, xy_corr):
     # xy is ndarray (x,y)
     # xy_corr is ndarray [(x1,y1),...,(xn,yn)]
-    # Gives phi as ndarray for all i in x
+    # Gives phi as ndarray for all i in xy
     N = xy_corr.shape[0]
     phi = np.zeros(N)
     for i in range(N):
-        xy_minus_xycorri = np.linalg.norm(xy-xy_corr[i])
-        if xy_minus_xycorri == 0.0:
+        r = np.linalg.norm(xy-xy_corr[i])
+        if r == 0.0:
             phi[i] = -1.0 # Limit evaluates to -1
         else:
-            phi[i] = (xy_minus_xycorri**2) * np.log(xy_minus_xycorri)
+            phi[i] = (r**2) * np.log(r)
     return phi
 
+def check_thin_plate_splines_against_naive():
+    params_filepath = '{0}{1}'.format(president_img_dir, morph_params_filename)
+    morph_filenames, morph_correspondences_dict, morph_output_filename = read_morph(params_filepath)
+    if len(morph_filenames) != 2:
+        raise Exception('Can only morph between 2 images')
+    # These are xy
+    img1_corr = morph_correspondences_dict[morph_filenames[0]]
+    img2_corr = morph_correspondences_dict[morph_filenames[1]]
+    # Flip to yx to be rc
+    img1_corr[:,[0,1]] = img1_corr[:,[1,0]]
+    img2_corr[:,[0,1]] = img2_corr[:,[1,0]]
+
+    a = rbf_thin_plate_splines(img2_corr, img1_corr)
+    b = np.zeros(a.shape)
+    for i in range(b.shape[0]):
+        b[i,:] = rbf_thin_plate_splines_naive(img2_corr[i],img1_corr)
+    rbf_thin_plate_splines_naive(img2_corr,img1_corr)
+    print('Vectorized within {0} of naive'.format((np.sum(a-b))))
+check_thin_plate_splines_against_naive()
 # %%
 def solve_transform_params(xy_corr, xy_prime_corr, basis_function):
     N = xy_corr.shape[0]
@@ -153,10 +203,10 @@ def morph(params_filepath):
     # Need to use interpolation and define Tx and Ty for an arbitrary (x,y) now that we (probably) have the solved transform (Tx and Ty) parameters (need to compute rbf for arbitrary (x,y) then multiply by corresponding k, sum, and interpolate to get (x',y'))
 
     # T1 from img1 warped towards img2
-    kx1, px1, ky1, py1 = solve_transform_params(img1_corr, img2_corr, rbf_thin_plate_splines)
+    kx1, px1, ky1, py1 = solve_transform_params(img1_corr, img2_corr, rbf_thin_plate_splines_naive)
 
     # T2 from img2 warped towards img1
-    kx2, px2, ky2, py2 = solve_transform_params(img2_corr, img1_corr, rbf_thin_plate_splines)
+    kx2, px2, ky2, py2 = solve_transform_params(img2_corr, img1_corr, rbf_thin_plate_splines_naive)
 
     # May want to make t a passed in parameter
     t = np.linspace(0,1,5)
@@ -220,9 +270,9 @@ root_path = os.path.dirname(os.path.abspath(params_filepath))
 img1 = ski.img_as_float(io.imread(os.path.join(root_path, morph_filenames[0]), as_gray=True))
 img2 = ski.img_as_float(io.imread(os.path.join(root_path, morph_filenames[1]), as_gray=True))
 # Pad images to be same size, will add to ends of dimensions so correspondence locations still have same meaning
-final_image_size = np.maximum(img1.shape, img2.shape)
-img1 = pad_image_to_bigger_or_equal_size_at_dim_ends(img1, final_image_size)
-img2 = pad_image_to_bigger_or_equal_size_at_dim_ends(img2, final_image_size)
+final_img_size = np.maximum(img1.shape, img2.shape)
+img1 = pad_image_to_bigger_or_equal_size_at_dim_ends(img1, final_img_size)
+img2 = pad_image_to_bigger_or_equal_size_at_dim_ends(img2, final_img_size)
 
 
 # RBF based coordinate warping can be viewed as RBF interpolation that will allow sampling using coordinates of the image that is being morphed "towards" to get the coordinates of the original image to sample from such that the correspondences of the original align with the correspondences of the image that is being morphed "towards"
@@ -231,15 +281,15 @@ T1 = RBFInterpolator(img2_corr, img1_corr)
 T2 = RBFInterpolator(img1_corr, img2_corr) 
 
 # Just like interp2d but specific for evenly spaced grids
-img1_interp2d = RectBivariateSpline(np.arange(final_image_size[0]), np.arange(final_image_size[1]), img1)
-img2_interp2d = RectBivariateSpline(np.arange(final_image_size[0]), np.arange(final_image_size[1]), img2)
+img1_interp2d = RectBivariateSpline(np.arange(final_img_size[0]), np.arange(final_img_size[1]), img1)
+img2_interp2d = RectBivariateSpline(np.arange(final_img_size[0]), np.arange(final_img_size[1]), img2)
 
-final_img_idxs = np.indices(final_image_size).reshape((2,-1)).T
-T1_final_image = T1(final_img_idxs)
-T2_final_image = T2(final_img_idxs)
+final_img_idxs = np.indices(final_img_size).reshape((2,-1)).T
+T1_final_img = T1(final_img_idxs)
+T2_final_img = T2(final_img_idxs)
 
-new_img1 = img1_interp2d(T1_final_image[:,0], T1_final_image[:,1], grid=False).reshape(final_image_size)
-new_img2 = img2_interp2d(T2_final_image[:,0], T2_final_image[:,1], grid=False).reshape(final_image_size)
+new_img1 = img1_interp2d(T1_final_img[:,0], T1_final_img[:,1], grid=False).reshape(final_img_size)
+new_img2 = img2_interp2d(T2_final_img[:,0], T2_final_img[:,1], grid=False).reshape(final_img_size)
 
 
 # new_img1 = np.zeros(img1.shape)
@@ -252,6 +302,22 @@ plt.show()
 plt.figure()
 plt.imshow(new_img2, cmap='gray')
 plt.show()
+
+# %%
+t_vals = np.linspace(0,1,num=4)
+
+for t in t_vals:
+    c_corr = (1-t)*img1_corr + t*img2_corr
+    T1 = RBFInterpolator(c_corr, img1_corr) 
+    T2 = RBFInterpolator(c_corr, img2_corr) 
+    T1_final_img = T1(final_img_idxs)
+    T2_final_img = T2(final_img_idxs)
+
+    final_img = ((1-t)*img1_interp2d(T1_final_img[:,0], T1_final_img[:,1], grid=False) + t*img2_interp2d(T2_final_img[:,0], T2_final_img[:,1], grid=False)).reshape(final_img_size)
+
+    plt.figure()
+    plt.imshow(final_img, cmap='gray')
+    plt.show()
 
 # TODO 
 # Perform morphing:
